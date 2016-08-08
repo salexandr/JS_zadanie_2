dbf6dcde423235dc63a1cc4709bc866bc0025ad2
Alexandr <a.senia@yandex.ru>
2016-08-08 14:59:11 +0400

    JSzadanie2

---
+++ zadanie1/js/script.js
@@ -0,0 +1,36 @@
+﻿var x = prompt ('Введите число');
+var n = prompt ('Введите степень');
+var x = +x;
+var n = +n;
+    x = Math.floor(x);
+    n = Math.floor(n);
+
+if (n == 0){
+x = 1;
+alert (x);
+} else {
+  if (n == 1){
+  x=x;
+  alert (x);
+  } else {
+    if (n < 0){
+    var a = 1/pow (x, n);
+    } else {
+      a = pow (x, n);
+    }
+  }
+}
+
+function pow(x, n) {
+  if (n < 0){n = -n}
+  var c = x;
+  c = +c;
+  for (var i = 1; i < n; i++){ 
+  c = c*x;
+  }
+
+  return c;
+}
+
+console.log(a);
+
---
+++ zadanie2/js/script.js
@@ -0,0 +1,15 @@
+﻿var names = [];
+
+for (var i = 0; i < 5; i++) {
+names[i] = prompt ('Введите имя');
+}
+
+console.log (names);
+var a = prompt ('Найти имя');
+
+a == names[0] ? alert(a+', Вы успешно вошли') :
+a == names[1] ? alert(a+', Вы успешно вошли') :
+a == names[2] ? alert(a+', Вы успешно вошли') :
+a == names[3] ? alert(a+', Вы успешно вошли') :
+a == names[4] ? alert(a+', Вы успешно вошли') :
+alert('Произошла ошибка');
